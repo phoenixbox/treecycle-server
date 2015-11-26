2b40c7fae5b63816343adaf23a7d8256bcb14ba7
Shane Rogers <shane@repllabs.com>
2015-11-26 14:47:50 +0000

    Pass user ID back on creation serializer

--- app/serializers/v1/create_serializer.rb
+++ app/serializers/v1/create_serializer.rb
@@ -1,5 +1,7 @@
 module V1
   class CreateSerializer < ActiveModel::Serializer
-    attributes :access_token, :uuid
+    attributes :access_token,
+               :uuid,
+               :id
   end
 end
