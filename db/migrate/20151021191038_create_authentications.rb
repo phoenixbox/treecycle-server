57de4718e66dad78458224461feae90daef53896
Shane Rogers <shane@repllabs.com>
2015-12-14 19:05:34 +0000

    Authentications schema restrictions lifted

--- db/migrate/20151021191038_create_authentications.rb
+++ db/migrate/20151021191038_create_authentications.rb
@@ -3,10 +3,10 @@ class CreateAuthentications < ActiveRecord::Migration
     create_table :authentications do |t|
       t.string :uid, null: false
       t.string :provider, null: false
-      t.string :token, null: false
-      t.string :token_type, null: false
+      t.string :token
+      t.string :token_type
       t.integer :user_id, null: false
-      t.integer :expiration, null: false
+      t.integer :expiration
       t.timestamps null: false
     end
   end
